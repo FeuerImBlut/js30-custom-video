ab4c44095543054ad83231b2edf981b235c1c468
Alexander Golubovich <verrucktx@gmail.com>
2022-02-06 16:29:43 +0300

    feat: add some features

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 const video = document.querySelector('.video-player');
 const bigPlayButton = document.querySelector('.play-hover');
-const playbutton = document.querySelector('.play-button');
+const playButton = document.querySelector('.play-button');
 const progress = document.querySelector('#progress');
-const muteButton = document.querySelector('.mute-buton');
+const muteButton = document.querySelector('.mute-button');
 const volume = document.querySelector('#volume');
+let tempVolume = 0.5;
+
+const whiteColor = getComputedStyle(document.documentElement)
+.getPropertyValue('--white');
+const goldColor = getComputedStyle(document.documentElement)
+.getPropertyValue('--gold');
 
 function togglePlay() {
     if (video.paused) {
         video.play();
         bigPlayButton.classList.add('pause');
-        playbutton.classList.add('pause');
+        playButton.classList.add('pause');
+        updateProgressBar();
     }
     else {
         video.pause();
         bigPlayButton.classList.remove('pause');
-        playbutton.classList.remove('pause');
+        playButton.classList.remove('pause');
     }
 }
 
-// video.addEventListener('pause', () => {
-//     bigPlayButton.classList.add('pause');
-// });
+function updateVideoProgress(number) {
+    video.currentTime = number / 100 * video.duration;
+}
 
-// video.addEventListener('play', () => {
-//     bigPlayButton.classList.remove('pause');
-// });
+function updateProgressBar() {
+    while (!video.paused) {
+        setInterval(() => {
+            var value = Math.round(+video.currentTime/+video.duration*100);
+            progress.style.background = `linear-gradient(to right, ${goldColor} 0%, ${goldColor} ${value}%, ${whiteColor} ${value}%, ${whiteColor} 100%)`;
+            progress.value = value;
+            }, 100);
+            return;
+    }
+}
+
+function muteVolume() {
+
+}
 
 video.addEventListener('click', () => {
     togglePlay();
@@ -32,7 +50,32 @@ video.addEventListener('click', () => {
 
 progress.addEventListener('input', function() {
     const value = this.value;
-    this.style.background = `linear-gradient(to right, ${colorOne} 0%, ${colorOne} ${value}%, ${colorTwo} ${value}%, ${colorTwo} 100%)`;
-    videoUpdate(value);
-  })
+    this.style.background = `linear-gradient(to right, ${goldColor} 0%, ${goldColor} ${value}%, ${whiteColor} ${value}%, ${whiteColor} 100%)`;
+    updateVideoProgress(value);
+  });
+
+volume.addEventListener('input', function() {
+    if (video.volume == 0) {
+        muteButton.classList.remove('muted');
+    }
+    const value = this.value;
+    if (value == 0) {
+        muteButton.classList.add('muted');
+    }
+    this.style.background = `linear-gradient(to right, ${goldColor} 0%, ${goldColor} ${value}%, ${whiteColor} ${value}%, ${whiteColor} 100%)`;
+    video.volume = this.value / 100;
+})
+
+muteButton.addEventListener('click', function() {
+    if (!this.classList.contains('muted')) {
+        tempVolume = video.volume;
+        video.volume = 0;
+    }
+    else {
+        video.volume = tempVolume;
+    }
+    this.classList.toggle('muted');
+})
+
+video.volume = 0.5;
 
